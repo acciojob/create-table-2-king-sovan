b6a4f056a177b11c0790eac9701038cd9b37aba6
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-10-09 19:42:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function createTable() {
     //Write your code here
-  
+	let rows = prompt("Input number of rows");
+	
+	let table = document.getElementById("myTable");
+
+	table.innerHTML = "";
+
+	let cols = rows;
+
+	for (let i = 0; i < rows; i++) {
+		let tr = document.createElement("tr");
+
+		for (let j = 0; j < cols; j++) {
+			let td = document.createElement("td");
+
+			td.textContent = `Row-${i} Column-${j}`
+
+			tr.appendChild(td);
+		}
+
+		table.appendChild(tr);
+	}
 }
